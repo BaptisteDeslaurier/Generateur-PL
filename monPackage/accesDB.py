565ed23a2aae0815202b335bc5af2204625ad52b
BaptisteDeslaurier <baptiste.deslaurier@bts-malraux.net>
2015-06-08 11:05:02 +0200

    Modification epreuve E4

--- monPackage/accesDB.py
+++ monPackage/accesDB.py
@@ -11,17 +11,17 @@ Connexion à la base Postgresql
 import sqlalchemy
 
 #Création de la variable engine initialisé avec la connexion à la BDD
-engine = sqlalchemy.create_engine('postgresql://b.deslaurier:passe@172.16.99.2:5432/radio_libre')
+engine = sqlalchemy.create_engine('postgresql://b.deslaurier:passe@172.16.99.2:5432/bdeslaurier/radio')
 
 # la collection de métadonnées est stockée dans l'objet MetaData
 metadonnees = sqlalchemy.MetaData()
 
-# récupère dans table_morceaux la structure de la base de données
+# récupère dans table_morceaux la structure de la table morceaux de la base de données
 table_morceaux = sqlalchemy.Table('morceaux',metadonnees,
                                   sqlalchemy.Column('titre',sqlalchemy.String),
                                   sqlalchemy.Column('album',sqlalchemy.String),
-                                  sqlalchemy.Column('artiste',sqlalchemy.String),
-                                  sqlalchemy.Column('genre',sqlalchemy.String),
+                                  sqlalchemy.Column('artiste',sqlalchemy.Integer),
+                                  sqlalchemy.Column('genre',sqlalchemy.Integer),
                                   sqlalchemy.Column('sousgenre',sqlalchemy.String),
                                   sqlalchemy.Column('duree', sqlalchemy.Integer),
                                   sqlalchemy.Column('format',sqlalchemy.String),
@@ -29,8 +29,22 @@ table_morceaux = sqlalchemy.Table('morceaux',metadonnees,
                                   sqlalchemy.Column('chemin',sqlalchemy.String)
                                   )
 
+# récupère dans table_artiste la structure de la table artiste de la base de données
+table_artiste = sqlalchemy.Table('artiste',metadonnees,
+                                  sqlalchemy.Column('id',sqlalchemy.integer),
+                                  sqlalchemy.Column('libelle_first',sqlalchemy.String),
+                                  sqlalchemy.Column('libelle_second',sqlalchemy.String)
+                                  )
+
+# récupère dans table_artiste la structure de la table genre de la base de données
+table_genre = sqlalchemy.Table('genre',metadonnees,
+                                  sqlalchemy.Column('id',sqlalchemy.integer),
+                                  sqlalchemy.Column('libelle_first',sqlalchemy.String),
+                                  sqlalchemy.Column('libelle_second',sqlalchemy.String)
+                                  )
+
 # s définit la requête à effectuer
-s = sqlalchemy.select([table_morceaux])
+s = sqlalchemy.select([table_morceaux, table_artiste, table_genre])
 
 # défini conn qui établi la connection à la base de données
 conn = engine.connect()
